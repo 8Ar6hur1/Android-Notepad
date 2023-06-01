220c56c302e58559ecc623eb3f455179214e4666
8Ar6hur1 <temp2003late@gmail.com>
2023-06-01 20:56:41 +0300

    Fixing the startup

--- app/src/main/java/com/example/android_notepad/MainActivity.java
+++ app/src/main/java/com/example/android_notepad/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.android_notepad;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.WindowManager;
 
-import com.example.android_notepad.screens.NotepadFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,12 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        NotepadFragment fragment = new NotepadFragment();
-        fragmentTransaction.replace(R.id.fragment_container, fragment);
-        fragmentTransaction.commit();
-
+        //
 
     }
 }
