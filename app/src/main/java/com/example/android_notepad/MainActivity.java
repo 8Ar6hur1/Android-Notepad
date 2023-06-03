e1745456915ceb5bf5a6ddfa3182b59962e96f17
8Ar6hur1 <temp2003late@gmail.com>
2023-06-04 00:36:43 +0300

    Added an activation for fap_add

--- app/src/main/java/com/example/android_notepad/MainActivity.java
+++ app/src/main/java/com/example/android_notepad/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         public void onLongClick(Notes notes, CardView cardView) {
 
         }
-    }
+    };
 }
 
 
--- app/src/main/java/com/example/android_notepad/NotesTakerActivity.java
+++ app/src/main/java/com/example/android_notepad/NotesTakerActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class NotesTakerActivity extends AppCompatActivity {
+
+    EditText editText_title, editText_notes;
+    ImageView imageView_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,10 @@ public class NotesTakerActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_notes_taker);
+
+        imageView_save = findViewById(R.id.imageView_save);
+        editText_title = findViewById(R.id.editText_title);
+        editText_notes = findViewById(R.id.editText_notes);
+
     }
 }
